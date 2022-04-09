f80592598650dbc3883c02af97a513587008d3fa
Danak-UY <m.claderaa@gmail.com>
2022-04-09 17:41:13 -0300

    feat: add new constants values

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,8 @@ const CONFIG_PATH = "./step-url.config.json";
 const QUERY_PARAM = "%s";
 const SPLIT_PARAM = "&";
 const JOIN_PARAM = " > ";
+const SEPARATOR_PARAM = " | ";
+const QUERY_SEPARATOR = " # ";
 
 const PARAMS = {
   name: "_name",
@@ -14,11 +16,21 @@ const URL_REGEX = new RegExp(
   "(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})"
 );
 
+const STATUS = {
+  OK: 200,
+  MISSING: 400,
+  NOT_FOUND: 404,
+  ERROR: 500,
+};
+
 module.exports = {
   CONFIG_PATH,
   SPLIT_PARAM,
   QUERY_PARAM,
   JOIN_PARAM,
+  SEPARATOR_PARAM,
+  QUERY_SEPARATOR,
   PARAMS,
   URL_REGEX,
+  STATUS,
 };
